31e8368086a4bf6b636b757e56a58158d7d4af3d
DylanVallejo <dylan1811vallejo@gmail.com>
2023-01-11 17:20:10 -0500

    Añadiendo el metodo get mediante  axios y usando useEffect para controlar una sola solicitud a la api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-// import Main from './components/Main';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
         <h1>Hello Kruger Star</h1>
-        {/* <Main/> */}
+        <Main/>
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,60 @@
-import { useDispatch,useSelector } from 'react-redux'
-import { fetchData } from '../features/data/dataSlice'
+// import { useDispatch,useSelector } from 'react-redux'
+// import { fetchData } from '../features/data/dataSlice'
+import axios from 'axios';
+import { useState,useEffect} from 'react';
 
 function Main() {
+   
+    const [fetchInfo, setFetchInfo] = useState([])
+    const [uno, setUno] = useState(1)
+    const key= '3vIRJmcLMjl1pQ0peu0exjGVox0U3cK8bUgWRrJC'
     
-    const data = useSelector((state) => state.data.value)
-    const dispatch = useDispatch();
+    const mainUrl= `https://api.nasa.gov/planetary/apod?api_key=${key}&count=1`
+    
+    // const data = useSelector((state) => state.data.value)
+    // const dispatch = useDispatch();
 
-    const handleValue = () =>{
-        dispatch(fetchData())
-    }
-    console.log(data)
+    // const handleValue = () =>{
+    //     dispatch(fetchData())
+    // }
+
+    
+    useEffect(()=>{
+        
+        axios.get(mainUrl)
+            .then(res=>{
+                setFetchInfo(res.data)
+                console.log(res)
+                
+                console.log(fetchInfo)
+            })      
+            .catch(err=>{
+                console.log(err)    
+            })  
+            
+    }, [uno]);
+
     
     return (
         <div>
-        
-            <button onClick={handleValue}>
-                aumentar
-            </button>
+            <div>
+                <h4>Fetch de la data solo un objeto</h4>
+                {fetchInfo.map((item)=>{
+                    return(
+                        <div className=' w-60 bg-white shadow rounded'>
+                            <h2 className='font-bold text-s mb-2'>{item.title}</h2>
+                            <p>{item.date}</p>
+                            <img className='w-full' alt={item.title} src={item.hdurl} />
+                            <p text-gray-700 text-base>{item.explanation}</p>
+                            <figcaption>{item.copyright}</figcaption>
+                            <div>
+                                <h1>conceptos</h1>
+                                {/* <span>{item.concept_tags}</span> */}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
