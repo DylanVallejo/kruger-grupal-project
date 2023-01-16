e293367ad3fd67952f8b5063648abdfd05b1c796
Edwin Cacuango <edwin-fernando@hotmail.es>
2023-01-16 00:49:44 -0500

    Estilos para singup singin pages usando layout

--- src/pages/SingIn.jsx
+++ src/pages/SingIn.jsx
@@ -30,50 +30,52 @@ const SingIn = () => {
     }
   };
   return (
-    <Layout aditionalStyles="text-center">
-      <h1 className="my-4 text-4xl">Inicia sesión</h1>
-      <form
-        onSubmit={formHandler}
-        className="grid place-content-center my-6 px-3 py-6 mx-auto border border-gray-200 shadow max-w-[20rem] md:max-w-[30rem] rounded-md text-center"
-      >
-        <div className="flex flex-col gap-2">
-          <label htmlFor="email" className="self-start">
-            Email
-          </label>
-          <input
-            name="email"
-            id="email"
-            className="border rounded px-2 py-1 w-60 md:w-80"
-            onChange={emailHandler}
-            value={email}
-            type="email"
-          />
-        </div>
-        <div className="flex flex-col gap-2 my-2">
-          <label htmlFor="password" className="self-start">
-            Contraseña
-          </label>
-          <input
-            className="border rounded px-2 py-1 md:w-80"
-            value={password}
-            onChange={passwordHandler}
-            type="password"
-            id="password"
-          />
-        </div>
-        <button className="bg-sky-500 p-2 rounded-lg text-white my-4">
-          Iniciar sesión
-        </button>
-      </form>
-      <p>
-        ¿Todavía no tienes cuenta? Crea una{" "}
-        <NavLink
-          to="/sing-up"
-          className="text-blue-900 underline underline-offset-1"
+    <Layout aditionalStyles="text-center text-white  min-h-screen min-w-screen">
+      <div className="flex flex-col items-center justify-center my-10 md:my-24">
+        <h1 className="my-4 text-4xl">Inicia sesión</h1>
+        <form
+          onSubmit={formHandler}
+          className="grid place-content-center my-6 px-3 py-6 mx-auto border border-gray-200 shadow max-w-[20rem] md:max-w-[30rem] rounded-md text-center"
         >
-          aquí
-        </NavLink>
-      </p>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email" className="self-start">
+              Email
+            </label>
+            <input
+              name="email"
+              id="email"
+              className="border rounded px-2 py-1 w-60 md:w-80"
+              onChange={emailHandler}
+              value={email}
+              type="email"
+            />
+          </div>
+          <div className="flex flex-col gap-2 my-2">
+            <label htmlFor="password" className="self-start">
+              Contraseña
+            </label>
+            <input
+              className="border rounded px-2 py-1 md:w-80"
+              value={password}
+              onChange={passwordHandler}
+              type="password"
+              id="password"
+            />
+          </div>
+          <button className="bg-sky-500 p-2 rounded-lg text-white my-4">
+            Iniciar sesión
+          </button>
+        </form>
+        <p>
+          ¿Todavía no tienes cuenta? Crea una{" "}
+          <NavLink
+            to="/sing-up"
+            className="text-slate-300 underline underline-offset-1"
+          >
+            aquí
+          </NavLink>
+        </p>
+      </div>
     </Layout>
   );
 };
