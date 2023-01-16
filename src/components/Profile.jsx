8fe589b5e5a97fb6a6a5024b39bd395cd306c47e
Edwin Cacuango <edwin-fernando@hotmail.es>
2023-01-16 02:31:05 -0500

    Fixed bug when clicked iniciar sesion button in profile compnt

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -85,15 +85,17 @@ const Profile = ({ addstyles }) => {
                       >
                         Regresar
                       </button>
-                      <button
-                        onClick={() => setShowModal(false)}
+                      <NavLink
+                        to="/sing-in"
                         className="bg-emerald-500 text-white active:bg-emerald-600
                       font-bold uppercase text-sm px-6 py-3 rounded shadow
                       hover:shadow-lg outline-none focus:outline-none mr-1 mb-1
                       ease-linear transition-all duration-150"
                       >
-                        <NavLink to="/sing-in">Iniciar sesión</NavLink>
-                      </button>
+                        <button onClick={() => setShowModal(false)}>
+                          Iniciar sesión
+                        </button>
+                      </NavLink>
                     </div>
                   </>
                 )}
