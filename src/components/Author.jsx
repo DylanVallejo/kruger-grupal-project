2a08ef02dde238569484ee8974f36cdec023d3ec
DylanVallejo <dylan1811vallejo@gmail.com>
2024-01-12 00:55:24 -0500

    retiro de imagenes en el modulo about

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -3,11 +3,11 @@ import { AiOutlineGithub } from "react-icons/ai";
 const Author = ({ item }) => {
   return (
     <div className="border shadow-lg rounded-md">
-      <img
+      {/* <img
         src={item.image}
         alt={item.name}
         className="h-1/2 object-cover w-full"
-      />
+      /> */}
       <div className="px-4 my-4">
         <h3 className="my-2 text-xl md:text-2xl">{item.name}</h3>
         <p className="md:text-lg">👨🏻‍💻 Programador</p>
